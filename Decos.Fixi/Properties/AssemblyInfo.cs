528236bc6cb8f405dfb97f7384c568f5d9c82be4
Nevin Javiya <nevin.javiya@decos.com>
2023-11-17 12:41:33 +0530

    Update assembly version

--- Decos.Fixi/Properties/AssemblyInfo.cs
+++ Decos.Fixi/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision
 // Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.48.0.0")]
-[assembly: AssemblyFileVersion("0.48.0.0")]
-[assembly: AssemblyInformationalVersion("0.48.0")]
+[assembly: AssemblyVersion("0.49.0.0")]
+[assembly: AssemblyFileVersion("0.49.0.0")]
+[assembly: AssemblyInformationalVersion("0.49.0")]
 [assembly: InternalsVisibleTo("Decos.Fixi.Tests")]
